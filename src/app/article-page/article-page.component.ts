9534e658d2ba3b980704f4416bb299eab4bc201d
bsao17 <declic62@gmail.com>
2021-01-30 10:21:04 +0100

    show comments by articles

--- src/app/article-page/article-page.component.ts
+++ src/app/article-page/article-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
+import {Globalservices} from '../service/globalservices';
 
 @Component({
   selector: 'app-article-page',
@@ -8,24 +9,32 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./article-page.component.css']
 })
 export class ArticlePageComponent implements OnInit {
-  id: number;
+  idBillet: number;
+  comments: any[];
+  commentsNumber: number;
+  commentContent: any;
+  commentDate: Date;
   dataApiTitle: string;
   dataApiContent: object;
   dataApiAuthors: string;
   dataApiDateTime: string;
   dataApiImg: string;
+  responseUser: any;
 
-  constructor(private router: Router, public httpClient: HttpClient) { }
+  constructor(private router: Router, public httpClient: HttpClient, public globalServices: Globalservices) {
+  }
 
   ngOnInit(): void {
     const url: string = this.router.url;
     const urlArray: any[] = url.split('/');
-    // tslint:disable-next-line:radix
-    this.id = parseInt(urlArray[3]);
+    this.idBillet = Number(urlArray[3]);
     this.getDataById();
+    this.getIdBilletOfComments();
+    this.getUsers();
   }
+  // GET Billets from API
   getDataById(): any{
-    this.httpClient.get(`https://jmpmapi.herokuapp.com/api/billets/${this.id}`)
+    this.httpClient.get(`https://jmpmapi.herokuapp.com/api/billets/${this.idBillet}`)
       .subscribe(
         (response) => {
           // @ts-ignore
@@ -43,5 +52,34 @@ export class ArticlePageComponent implements OnInit {
         (error) =>  console.log(error),
         () => console.log('complete')
       );
+  }
+  getIdBilletOfComments(): any{
+    const requestComment = this.httpClient.get(`https://jmpmapi.herokuapp.com/api/billets/${this.idBillet}/comments`);
+    requestComment.subscribe(
+      comments => {
+            this.comments = comments['hydra:member'];
+            this.commentContent = comments['hydra:member'][0].content;
+            this.commentDate = comments['hydra:member'][0].date;
+            console.log('le nombre d\'instances est de ' + this.comments.length + ' commentaires pour cet articles');
+      },
+      (error) => console.log(error),
+      () => console.log('Complete')
+    );
+  }
+  getCo(): any{
+  }
+  // GET User from API
+  getUsers(): any{
+    this.httpClient.get(`https://jmpmapi.herokuapp.com/api/users/${this.idBillet}`).subscribe(
+      response => {
+        this.responseUser = {
+          // @ts-ignore
+          email: response.email,
+        };
+        console.log(response);
+      },
+      (error) => console.log(error),
+      () => console.log('Complete')
+    );
   }
 }
--- src/app/service/globalservices.ts
+++ src/app/service/globalservices.ts
@@ -1,21 +1,37 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class Globalservices {
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+  }
   data: object[];
   dataId: any;
-  datapi: any;
+  datapi: any[];
+  comments: any;
+  singleComment: any;
+  id: any;
   onGetData(): any{
     this.httpClient.get(`https://jmpmapi.herokuapp.com/api/billets`).subscribe((response) => {
       this.data = [response];
       this.datapi = this.data[0]['hydra:member'];
-      console.log(this.datapi);
     },
       (error) => { console.log('Erreur de chargement serveur ...' + error); },
       () => { console.log('Complete'); }
+    );
+  }
+
+  getComments(id: number): any{
+    this.httpClient.get(`https://jmpmapi.herokuapp.com/api/comments/`).subscribe(
+      comments => {
+        this.comments = comments['hydra:member'][id];
+        this.singleComment = comments['hydra:member'];
+        console.log('log GlobalServoces', this.comments, id);
+      },
+      (error) => console.log(error),
+      () => console.log('Complete')
     );
   }
 }
